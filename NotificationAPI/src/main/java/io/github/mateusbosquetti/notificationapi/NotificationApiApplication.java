0543b961fd10ecccab59f29b93da35ab3a8eb557
Mateus Henrique Bosquetti <mateushb123@gmail.com>
2025-12-16 19:07:28 -0300

    feat: notification API

--- NotificationAPI/src/main/java/io/github/mateusbosquetti/notificationapi/NotificationApiApplication.java
+++ NotificationAPI/src/main/java/io/github/mateusbosquetti/notificationapi/NotificationApiApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class NotificationApplication {
+public class NotificationApiApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(NotificationApplication.class, args);
+        SpringApplication.run(NotificationApiApplication.class, args);
     }
 
 }
