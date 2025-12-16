92cef36a252b2ef0749730be649a2a0f486e9c61
Mateus Henrique Bosquetti <mateushb123@gmail.com>
2025-12-16 19:35:02 -0300

    feat: notification API producing data

---
+++ NotificationAPI/src/main/java/io/github/mateusbosquetti/notificationapi/model/dto/NotificationKafkaDTO.java
@@ -0,0 +1,4 @@
+package io.github.mateusbosquetti.notificationapi.model.dto;
+
+public record NotificationKafkaDTO(String message, Long userId) {
+}
--- NotificationAPI/src/main/java/io/github/mateusbosquetti/notificationapi/service/NotificationService.java
+++ NotificationAPI/src/main/java/io/github/mateusbosquetti/notificationapi/service/NotificationService.java
@@ -1,22 +1,32 @@
 package io.github.mateusbosquetti.notificationapi.service;
 
+import io.github.mateusbosquetti.notificationapi.model.dto.NotificationKafkaDTO;
 import io.github.mateusbosquetti.notificationapi.model.entity.Notification;
 import io.github.mateusbosquetti.notificationapi.repository.NotificationRepository;
 import lombok.AllArgsConstructor;
+import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
+
+import java.util.UUID;
 
 @Service
 @AllArgsConstructor
 public class NotificationService {
 
     private final NotificationRepository repository;
+    private final KafkaTemplate<String, Object> kafkaTemplate;
 
     public Notification createNotification (String message, Long userId) {
         Notification notification = Notification.builder()
                 .message(message)
                 .userId(userId)
                 .build();
-        return this.repository.save(notification);
+        notification = this.repository.save(notification);
+
+        NotificationKafkaDTO kafkaDTO = new NotificationKafkaDTO(notification.getMessage(), notification.getUserId());
+        kafkaTemplate.send("notification.sent", UUID.randomUUID().toString(), kafkaDTO);
+
+        return notification;
     }
 
 }
